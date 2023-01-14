40bfe720f2cd8403a93abb231c5b672c2f34219b
Gerardo Meyer <RTrodez@gmail.com>
2023-01-13 23:24:32 -0600

    Terminando el proyecto

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 // Promedio
-let alturaPromedio = []
-let pesoPromedio = []
-let sumaAltura = 0
+let myChart;
 
-console.log(alturaPromedio.length)
-
-// Random
+// Random número para personajes
 function getRandomInt() {
-    return Math.round(Math.floor(Math.random() * 82));
+    return Math.round(Math.floor(Math.random() * 81));
 }
-
-function getAltura(sumaAltura) {
-    for (i = 0; i < alturaPromedio.length; i++) {
-        sumaAltura += alturaPromedio[i]
-        //divisor += 1
-    }
-    return sumaAltura
-}
-
 
 // Api
 function clickMe() {
-    console.log(getRandomInt(82));
     let personaje = getRandomInt()
 
     fetch(`https://swapi.dev/api/people/${personaje}/`)
@@ -29,26 +15,113 @@ function clickMe() {
         .then((result) => {
             console.log('el resultado ', result);
             // AQUÍ CÓDIGO
+            let alturaPromedio = []
+            let pesoPromedio = []
+
+            // Peso
             let peso = parseFloat(result.mass)
+            if (isNaN(peso)) {
+                peso = 0;
+            }
+
+            // Altura
             let altura = parseFloat(result.height)
-            console.log(peso)
-            console.log(altura)
+
+            // Arrays de Peso y Altura
             alturaPromedio.push(altura)
-            // pesoPromedio.push(peso)
+            pesoPromedio.push(peso)
 
-            let numerador = getAltura(sumaAltura)
-            let divisor = alturaPromedio.length
+            // // Altura
+            // let numerador = getAltura(sumaAltura)
+            // let divisor = alturaPromedio.length
 
-            console.log(`Denominador ${divisor}`)
-            console.log(`Promedio: ${numerador / divisor}`)
-            // console.log(pesoPromedio)
+            // // Peso
+            // let numeradorPeso = getPeso(sumaPeso)
+            // let divisorPeso = pesoPromedio.length
+
+            // console.log(`Denominador ${divisor}`)
+            // console.log(`Promedio altura: ${numerador / divisor}`)
+            // console.log(`Promedio peso: ${numeradorPeso / divisorPeso}`)
+
+            // Cartas
+            let contenedorPadre = document.querySelector('.contenedorCartas')
+            let contenedorGrafico = document.querySelector('.containerGrafico')
+            contenedorPadre.innerHTML = ``
+            let salto = document.createElement('br')
+            let nuevaCarta = document.createElement('div')
+            nuevaCarta.setAttribute('class', 'card cartaPersonaje ')
+            nuevaCarta.style.width = '18rem'
+            nuevaCarta.innerHTML = `
+                        <div class="card-body">
+                            <h5 class="card-title">${result.name}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Gender: ${primeraLetraMayuscula(result.gender)}</h6>
+                            <ul class="card-text">
+                                <li><strong>Height:</strong> ${result.height}</li>
+                                <li><strong>Weight:</strong> ${result.mass}</li> 
+                            </ul>
+                            <a href="${result.homeworld}" class="card-link">Homeworld</a>
+                            </div>
+                            `
+            contenedorPadre.appendChild(nuevaCarta)
 
 
+            // Graficos JChart
+            // Obtener una referencia al elemento canvas del DOM
+            // Obtener una referencia al elemento canvas del DOM
+            const $grafica = document.querySelector(".graficaPrueba");
+            // Las etiquetas son las que van en el eje X. 
+            const etiquetas = ["Peso - Altura"]
+            // Podemos tener varios conjuntos de datos
+            const datosPeso = {
+                label: "Peso",
+                data: pesoPromedio, // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
+                borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
+                borderWidth: 1, // Ancho del borde
+            };
+            const datosAltura = {
+                label: "Altura",
+                data: alturaPromedio, // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                backgroundColor: 'rgba(255, 159, 64, 0.2)', // Color de fondo
+                borderColor: 'rgba(255, 159, 64, 1)', // Color del borde
+                borderWidth: 1, // Ancho del borde
+            };
 
+            if (myChart) {
+                myChart.destroy();
+            }
+
+            myChart = new Chart($grafica, {
+                type: 'bar', // Tipo de gráfica
+                data: {
+                    labels: etiquetas,
+                    datasets: [
+                        datosPeso,
+                        datosAltura,
+                        // Aquí más datos...
+                    ]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }],
+                    },
+                }
+            });
 
 
         })
         .catch(error => {
-            console.log('no existe tu personaje', error);
+            console.log('No existe tu personaje', error);
         })
+}
+
+// Primer letra mayúscula función
+let primeraLetraMayuscula = (cadena) => {
+    let primerCaracter = cadena.charAt(0).toUpperCase();
+    let restoDeLaCadena = cadena.substring(1, cadena.length);
+    return primerCaracter.concat(restoDeLaCadena);
 }
